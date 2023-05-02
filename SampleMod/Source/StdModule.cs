21f967028ee77faf8c93df2b24e9c5d0a07c0dbf
TheMysticLynx <57817285+TheMysticLynx@users.noreply.github.com>
2023-05-01 20:02:58 -0700

    fix namespace

--- SampleMod/Source/StdModule.cs
+++ SampleMod/Source/StdModule.cs
@@ -1,38 +1,37 @@
 using System;
 using System.Collections.Generic;
+using Indev2;
 using Modding;
 
-namespace Indev2
+
+//MUST BE NAMED MOD
+public class Mod : IMod
 {
-    //MUST BE NAMED MOD
-    public class Mod : IMod
+    public static Interface Interface;
+    public string UniqueName => "Trover Mod";
+    public string DisplayName => "Trover Mod";
+    public string Author => "Mystic";
+    public string Version => "5.0";
+    public string Description => "Trover Mod";
+    public string[] Dependencies => Array.Empty<string>();
+
+    public void Initialize(Modding.Interface @interface)
     {
-        public static Interface Interface;
-        public string UniqueName => "Trover Mod";
-        public string DisplayName => "Trover Mod";
-        public string Author => "Mystic";
-        public string Version => "5.0";
-        public string Description => "Trover Mod";
-        public string[] Dependencies => Array.Empty<string>();
+        Interface = @interface;
+    }
 
-        public void Initialize(Modding.Interface @interface)
-        {
-            Interface = @interface;
-        }
-
-        public IEnumerable<CellProcessor> GetCellProcessors(ICellGrid cellGrid)
-        {
-            yield return new BasicCellProcessor(cellGrid);
-            yield return new SlideCellProcessor(cellGrid);
-            yield return new FreezeProcessor(cellGrid);
-            yield return new GeneratorCellProcessor(cellGrid);
-            yield return new CWRotateProcessor(cellGrid);
-            yield return new CCWRotateProcessor(cellGrid);
-            yield return new TroverCellProcessor(cellGrid);
-            yield return new MoverCellProcessor(cellGrid);
-            yield return new WallCellProcessor(cellGrid);
-            yield return new TrashCellProcessor(cellGrid);
-            yield return new EnemyCellProcessor(cellGrid);
-        }
+    public IEnumerable<CellProcessor> GetCellProcessors(ICellGrid cellGrid)
+    {
+        yield return new BasicCellProcessor(cellGrid);
+        yield return new SlideCellProcessor(cellGrid);
+        yield return new FreezeProcessor(cellGrid);
+        yield return new GeneratorCellProcessor(cellGrid);
+        yield return new CWRotateProcessor(cellGrid);
+        yield return new CCWRotateProcessor(cellGrid);
+        yield return new TroverCellProcessor(cellGrid);
+        yield return new MoverCellProcessor(cellGrid);
+        yield return new WallCellProcessor(cellGrid);
+        yield return new TrashCellProcessor(cellGrid);
+        yield return new EnemyCellProcessor(cellGrid);
     }
 }
